7b0be2488ef27f6086ab97ff52a6ed1eedeab901
Nikita Zubov <nikzubov770@mail.ru>
2023-03-06 19:58:41 +0300

    Add project

---
+++ I-am-Poor/I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Nikita Zubov on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
